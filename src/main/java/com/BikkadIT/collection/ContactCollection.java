291776511a094b15c166479727122c35e63ca140
sumitchahare <chaharesumit@gmail.com>
2022-08-24 01:34:38 +0530

    PHON-2:create required package Structure

---
+++ src/main/java/com/BikkadIT/collection/ContactCollection.java
@@ -0,0 +1,8 @@
+package com.BikkadIT.collection;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ContactCollection {
+
+}
---
+++ src/main/java/com/BikkadIT/service/ContactServiceI.java
@@ -0,0 +1,5 @@
+package com.BikkadIT.service;
+
+public interface ContactServiceI {
+
+}
